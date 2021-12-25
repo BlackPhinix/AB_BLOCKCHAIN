c353f30f7df62b226a4e441b36c6edb3455ceea2
Abena <72131115+BlackPhinix@users.noreply.github.com>
2021-12-25 14:29:10 +0300

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,43 @@
+#write a simple for loop in python
+    
+#import the datetime module
+import datetime
+#create a variable called now
+now = datetime.datetime.now()
+#print the current date and time
+print(now)
+#print the current year
+print(now.year)
+#print the current month
+print(now.month)
+#print the current day
+print(now.day)
+#print the current hour
+print(now.hour)
+#print the current minute
+print(now.minute)
+#print the current second
+print(now.second)
+#print the current microsecond
+print(now.microsecond)
+#print the current timezone
+print(now.tzinfo)
+#print the current date and time
+print(now.date())
+#print the current date and time
+print(now.time())
+#print the current date and time
+print(now.timetz())
+#print the current date and time
+print(now.isoformat())
+#print the current date and time
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+#print the current date and time
+print(now.strftime("%Y-%m-%d %H:%M:%S.%f"))
+#print the current date and time
+#end the current code
+print("end")
+#end the current code
+
+
+
